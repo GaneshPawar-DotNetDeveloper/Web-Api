d4fe9c287139a3840ab47d17a9e369fa54c4cc34
SurajDotNetDeveloper <surajcool9623@gmail.com>
2024-07-12 05:10:06 +0530

    Add project files.

---
+++ APIDemo/Controllers/StudentsController.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using APIDemo.Models;
+
+namespace APIDemo.Controllers
+{
+    public class StudentsController : ApiController
+    {
+        private WebApiEntities db = new WebApiEntities();
+
+        // GET: api/Students
+        public IQueryable<Student> GetStudents()
+        {
+            return db.Students;
+        }
+
+        [HttpGet]
+        // GET: api/Students/5
+        [ResponseType(typeof(Student))]
+        public IHttpActionResult GetStudent([FromUri]int id)
+        {
+            Student student = db.Students.Find(id);
+            if (student == null) 
+            {
+                return NotFound();
+            }
+
+            return Ok(student);
+        }
+
+        // PUT: api/Students/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutStudent(int id, Student student)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != student.Rollnumber)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(student).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!StudentExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Students
+        [ResponseType(typeof(Student))]
+        public IHttpActionResult PostStudent(Student student)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Students.Add(student);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = student.Rollnumber }, student);
+        }
+
+        // DELETE: api/Students/5
+        [ResponseType(typeof(Student))]
+        public IHttpActionResult DeleteStudent(int id)
+        {
+            Student student = db.Students.Find(id);
+            if (student == null)
+            {
+                return NotFound();
+            }
+
+            db.Students.Remove(student);
+            db.SaveChanges();
+
+            return Ok(student);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool StudentExists(int id)
+        {
+            return db.Students.Count(e => e.Rollnumber == id) > 0;
+        }
+    }
+}
---
+++ APIdemo2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="APIdemo2.WebApiApplication" Language="C#" %>
