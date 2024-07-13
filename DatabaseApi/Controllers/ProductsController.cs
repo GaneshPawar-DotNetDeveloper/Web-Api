e60e9f0e29078cfe4e4ed0d585e2859f76268320
SurajDotNetDeveloper <surajcool9623@gmail.com>
2024-07-14 00:02:34 +0530

    swagger testing

---
+++ DatabaseApi/Controllers/ProductsController.cs
@@ -0,0 +1,39 @@
+﻿using DatabaseApi.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+
+namespace DatabaseApi.Controllers
+{
+    [RoutePrefix("api/products")]
+    public class ProductsController : ApiController
+    {
+        private readonly AppDbContext _context;
+
+        public ProductsController()
+        {
+            _context = new AppDbContext();
+        }
+
+        // GET: api/products
+        [HttpGet]
+        [Route("")]
+        public IEnumerable<Product> GetProducts()
+        {
+            return _context.Products.ToList();
+        }
+
+        // GET: api/products/5
+        [HttpGet]
+        [Route("{id:int}")]
+        public IHttpActionResult GetProduct(int id)
+        {
+            var product = _context.Products.Find(id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return Ok(product);
+        }
+    }
+}
---
+++ StudentApiClient/Models/Student.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace StudentApiClient.Models
+{
+    public class Student
+    {
+        public int Rollnumber { get; set; }
+        public string Name { get; set; }
+        public string Gender { get; set; }
+    }
+}
